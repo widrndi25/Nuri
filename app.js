041c9338e606337d1690d23b5f3dd8d6736c87da
Lee JeongHyo <widrndi2@naver.com>
2023-06-06 10:24:04 +0900

    Update: something change

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 function scrollHandler() {
-  // 스크롤 위치를 가져옵니다.
   var scrollPosition = window.scrollY || window.pageYOffset;
-
   var element = document.getElementById("intro");
+
   if (scrollPosition > 200) {
     element.classList.add("introAnimation");
   }
